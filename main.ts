695c3961537f1ee5af168f9fc3fbe43387895777
Sebastian009w <ia3266595@gmail.com>
2023-04-11 22:41:41 -0500

    changes minimos

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import { Application } from "$x/oak@v12.1.0/mod.ts";
 
 
 (async () => {
-    console.log("http://localhost:8080")
     const app = new Application();
     app.use((ctx) => {
         ctx.response.body = {
             deno: "deploy",
         };
     });
-  await app.listen({ port: 8080 });
+    await app.listen({ port: 8080 });
+    console.log("http://localhost:8080")
 })();
