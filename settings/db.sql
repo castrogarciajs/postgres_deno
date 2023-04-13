69321d315f05c375d11d08f221942bd790c40e67
Sebastian009w <ia3266595@gmail.com>
2023-04-13 14:21:03 -0500

    init testing rest Deno

--- settings/db.sql
+++ settings/db.sql
@@ -12,3 +12,5 @@ SELECT * FROM task_deno;
 INSERT INTO task_deno(name,lastname) VALUES ('Sebastian', 'Castro');
 
 DELETE FROM task_deno WHERE id = 1;
+
+UPDATE task_deno SET name="Sebastian" lastname="Castro" WHERE id=2
