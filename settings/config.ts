135d1ac2ebaa15e9d78a5e4e7b43ba474e95d29f
Sebastian009w <ia3266595@gmail.com>
2023-04-12 23:20:47 -0500

    cambios

--- settings/config.ts
+++ settings/config.ts
@@ -2,7 +2,6 @@ import { config } from "$x/dotenv@v3.2.2/mod.ts";
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOSTNAME } = config();
 
-/*** @config */
 export const DATABASE_CONFIG = {
   user: DB_USERNAME,
   database: DB_NAME,
